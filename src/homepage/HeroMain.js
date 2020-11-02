c914616da34618d15f1043d1019af32e4ada8c4f
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-11-02 12:31:48 -0600

    Added text for the hero

--- src/homepage/HeroMain.js
+++ src/homepage/HeroMain.js
@@ -3,7 +3,10 @@ import React from 'react'
 function HeroMain(){
     return(
         <section className="Hero-main">
-           
+           <h1>Track your nutrient intake with 
+               <span>Nutrimeasure</span>
+           </h1>
+           <button>GET STARTED</button>
         </section>
     )
 
