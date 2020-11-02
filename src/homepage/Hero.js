6a10bff68ea842b3dac3b17b9f182230f34ba0f4
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-11-02 11:22:12 -0600

    Created Hero componentfor homepage

---
+++ src/homepage/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Hero(){
+    return (
+        <header>
+            <p>Nutrimeasure</p>
+            <a href="#" >LOGIN</a>
+        </header>
+    )
+}
+
+export default Hero
