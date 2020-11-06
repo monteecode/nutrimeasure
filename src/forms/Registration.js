f381c2a5118772c4d86b7137dd2f5ea68ffd60f7
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-11-05 20:39:08 -0600

    Create a registration form

---
+++ src/forms/Registration.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react'
+import React, {Component} from 'react'
+
+class Registraton extends Component {
+    state = {
+        firstName: '',
+        lastName: ''
+    }
+
+    render(){
+        return(
+            
+        )
+    }
+
+}
+
+export default Registration
