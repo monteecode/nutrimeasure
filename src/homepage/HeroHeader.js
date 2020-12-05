3ec7260ef4de90601d28befaa45ab906cb449a56
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-12-04 20:42:54 -0600

    Add Routes to link login and registration forms

--- src/homepage/HeroHeader.js
+++ src/homepage/HeroHeader.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Link, Route, Switch} from 'react-router-dom'
+import Login from '../forms/Login'
 
 function HeroHeader(){
     return (
         <header className="Hero-header">
-            <p>Nutrimeasure</p>
-            <a href="#" >LOGIN</a>
+            <Link to="/">Nutrimeasure</Link>
+            <Link to="/login" >LOGIN</Link>
+            <Switch>
+                <Route exact path="/">
+                    
+                </Route>
+                <Route exact path="/login">
+                    <Login />
+                </Route>
+            </Switch>
         </header>
         
     )
--- src/homepage/hero/RightHero.js
+++ src/homepage/hero/RightHero.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom'
+import Registration from '../../forms/Registration'
 
 const RightHero = () => {
     return (
         <div className="Right-hero">
-            <button>GET STARTED</button>
+            <button>
+                <Link to='/forms/registration'>GET STARTED</Link>
+            </button>
+
+            <Switch>
+                <Route path='/forms/registration'>
+                    <Registration />
+                </Route>
+            </Switch>
         </div>
     )
 }
