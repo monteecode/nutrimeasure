5ab32559240ef5bed7a699568ff01fd17b7ea5c5
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-12-04 20:13:09 -0600

    Add React DOM package and fix export mispelling for Registration component

--- src/forms/Registration.js
+++ src/forms/Registration.js
@@ -1,7 +1,6 @@
-import { render } from '@testing-library/react'
 import React, {Component} from 'react'
 
-class Registraton extends Component {
+class Registration extends Component {
     state = {
         firstName: '',
         lastName: '',
