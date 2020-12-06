acb171d7dcac6d33d2ea12ce01c2fbf148450970
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-12-06 16:57:29 -0600

    Fix route for login and register links on homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import Homepage from './homepage/Homepage';
 import Registration from './forms/Registration'
+import Login from './forms/Login'
 
 function App() {
   return (
@@ -11,10 +12,12 @@ function App() {
         <Route exact path='/'>
           <Homepage />
         </Route>
-        <Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/register'>
           <Registration />
         </Route>
-
       </Switch>
     </div>
   );
--- src/homepage/HeroNav.js
+++ src/homepage/HeroNav.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import {Link, Route, Switch} from 'react-router-dom'
-import Login from '../forms/Login'
+import {Link} from 'react-router-dom'
+
 
 function HeroNav(){
     return (
         <header className="Hero-header">
             <Link to="/">Nutrimeasure</Link>
             <Link to="/login" >LOGIN</Link>
-            <Switch>
-                <Route exact path="/">
-                   
-                </Route>
-                <Route exact path="/login">
-                    <Login />
-                </Route>
-            </Switch>
         </header>
         
     )
